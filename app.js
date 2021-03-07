1841bb975e2da3f0048a6443b828e93039d6686b
hashimAslam <hashim14aslam@gmail.com>
2021-03-07 14:53:15 +0530

    fix:001

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ window.addEventListener(
   () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
       fetchVideos();
     }
   },
@@ -82,7 +82,7 @@ window.addEventListener(
   () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-    if (scrollTop + clientHeight >= scrollHeight) {
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
       fetchVideos();
     }
   },
